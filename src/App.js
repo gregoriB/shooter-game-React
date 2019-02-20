b0dbe88771884e9f8d99391c091e2c41d83ad1dd
gregoriB <brandon.gregori@gmail.com>
2019-02-19 23:44:23 -0800

    working on movement interval issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Arena from './components/world/Arena';
+// import Arena from './components/world/Arena';
+import Player from './components/player/Player';
 
 const App = () => {
   return (
     <div className="App">
-      <Arena />
+      <Player />
     </div>
   );
 }
